cdfc90137bd6149ad869fe8872a8af3cf5b1cd7f
Christopher <chrismartinez.2054@gmail.com>
2023-08-19 23:00:49 -0600

    import, export components

---
+++ src/Greeting.js
@@ -0,0 +1,7 @@
+export function Greeting() {
+    return <h1>Componente de React</h1>
+  }
+
+  export function Hello() {
+    return <p>Hello</p>
+  }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import { Greeting, Hello } from "./Greeting";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-function Greeting() {
-  const user = {
-    firstName: "Pedro",
-    lastName: "Juarez"
-  }
-  return <div>
-    <h1>{user.firstName}</h1>
-    <h2>{user.lastName}</h2>
-
-  </div>
-}
-root.render(<Greeting />);
+root.render(
+  <>
+    <Greeting />
+    <Hello />
+  </>
+);
