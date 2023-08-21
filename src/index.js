e499500b19fa3bec88e5b51ccc79ef43069e0f43
Christopher <chrismartinez.2054@gmail.com>
2023-08-20 22:05:01 -0600

    component with class

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Greeting, Hello } from "./Greeting";
+import {Saludar} from './Saludar';
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
     <Greeting />
     <Hello />
+    <Saludar/>
   </>
 );
