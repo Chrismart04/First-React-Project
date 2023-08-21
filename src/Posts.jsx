0c6aff87e643af2beaa3267304c13c17abd82e4f
Christopher <chrismartinez.2054@gmail.com>
2023-08-20 23:45:30 -0600

    fetch using async await

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,13 +1,19 @@
 export function Posts() {
   return (
     <button
-      onClick={() => {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json())
-        .then(data => console.log(data))
+      onClick={async () => {
+       try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const data = await response.json();
+        console.log(data);
+       } catch(error){
+        console.error('error:', error)
+       }
       }}
     >
       traer datos
     </button>
   );
 }
+
+
